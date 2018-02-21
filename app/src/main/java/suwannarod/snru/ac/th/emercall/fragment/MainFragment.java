8caf78a04f350b8adc8f48e034be7247379a1cc7
suwanna <suwanna.mu59@snru.ac.th>
2018-02-21 14:17:24 +0700

    Last

--- app/src/main/java/suwannarod/snru/ac/th/emercall/fragment/MainFragment.java
+++ app/src/main/java/suwannarod/snru/ac/th/emercall/fragment/MainFragment.java
@@ -1,11 +1,15 @@
 package suwannarod.snru.ac.th.emercall.fragment;
 
+import android.media.Image;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import suwannarod.snru.ac.th.emercall.R;
 
@@ -14,6 +18,50 @@ import suwannarod.snru.ac.th.emercall.R;
  */
 
 public class MainFragment extends Fragment{
+
+
+
+//    Explicit
+    private String tag = "MyTagV1";
+
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//     For Station1
+        ImageView station1ImageView = getView().findViewById(R.id.imageViewStation1);
+        station1ImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Log.d(tag,"You click Image Station1");
+
+            }
+        });
+
+        TextView station1TextView = getView().findViewById(R.id.textViewStation1);
+        station1TextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(tag,"Click Text" + getString(R.string.station1));
+            }
+        });
+
+//     For Station2
+
+//     For Station3
+
+//     For Station4
+
+
+
+   }  // Main Method
+
+    public void callstation(String numberCal) {
+
+    }
+
 
     @Nullable
     @Override
